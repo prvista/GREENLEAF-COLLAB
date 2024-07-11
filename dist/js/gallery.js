3ff7dbfd5d1f7a413ea19e998641f8d7b025f7b3
KenSumang <vistapaulrainier@gmail.com>
2024-07-11 15:40:45 +0800

    reinier push

---
+++ dist/js/gallery.js
@@ -0,0 +1,3 @@
+lightGallery(document.querySelector('.readmore__img'))
+lightGallery(document.querySelector('.overlay__readmore'))
+lightGallery(document.querySelector('.footer__gallery'))
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,11 @@ module.exports = {
         secondary: "#232323",
         tertiary: "#8ba216",
         light: "#ffffff",
+        pGray: "#9c9c9c;",
       },
-      
+      backgroundImage:{
+        menu:"linear-gradient(rgba(35, 35, 35, 0.94), rgba(35, 35, 35, 0.94)),  url('../img/bg-image.jpg')",
+      }
     },
   },
   plugins: [],
