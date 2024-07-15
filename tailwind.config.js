82410cc3a8749dee466e1963b0f2ab68b62c43b8
Mikegasti <avelpogi10@gmail.com>
2024-07-15 11:20:34 +0800

    paulo

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,13 +9,15 @@ module.exports = {
       },
 
       colors:{
+        green:"['rgba(94,240,49,0.8)', 'rgba(94,240,49,0.8)']",
         primary: "#9fba1a",
         secondary: "#232323",
         tertiary: "#8ba216",
         light: "#ffffff",
       },
       backgroundImage:{
-        team:"linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.6)), url('../img/lawn-768316_1920.jpg')",
+        team:"linear-gradient(rgba(165, 184, 48,1),rgba(165, 184, 48,0.4)), url('../img/lawn-768316_1920.jpg')",
+        about:"url('..img/page-title.jpg')",
       }
     },
   },
